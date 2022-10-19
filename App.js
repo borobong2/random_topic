3cb6a1d30f91531c6660aac75efa9277a9ac58c5
borobong2 <borobong230@gmail.com>
2022-10-19 21:04:37 +0900

    feat: Add random topic

--- App.js
+++ App.js
@@ -5,7 +5,19 @@ import CustomButton from './src/components/CustomButton';
 const App = () => {
   let [text, setText] = useState(0);
   const changeText = () => {
-    setText(getRandom(0, 10));
+    setText(getRandom(1, 10));
+  };
+  const data = {
+    topic1: '안녕',
+    topic2: '안녕2',
+    topic3: '안녕3',
+    topic4: '안녕4',
+    topic5: '안녕5',
+    topic6: '안녕6',
+    topic7: '안녕7',
+    topic8: '안녕8',
+    topic9: '안녕9',
+    topic10: '안녕10',
   };
   const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -14,7 +26,7 @@ const App = () => {
   return (
     <View style={{flex: 1}}>
       <CustomButton onPress={changeText} />
-      <Text style={{backgroundColor: 'red'}}>{text}</Text>
+      <Text style={{backgroundColor: 'red'}}>{data[`topic${text}`]}</Text>
     </View>
   );
 };
